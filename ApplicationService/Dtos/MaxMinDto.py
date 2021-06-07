f95c8cc41f2090ad36a1a00a99161a8b4193ded4
Kyrmasch <Kyrmasch@gmail.com>
2021-06-08 00:02:55 +0600

    Merge rows!

---
+++ ApplicationService/Dtos/MaxMinDto.py
@@ -0,0 +1,8 @@
+
+from ApplicationService.Dtos.SettingsUrlDto import SettingsUrlDto
+
+
+class MaxMinDto(object):
+    def __init__(self, max: int, min: int):
+        self.max = max
+        self.min = min
--- Utils/DataFrame.py
+++ Utils/DataFrame.py
@@ -1,17 +1,48 @@
+from ApplicationService.Dtos.MaxMinDto import MaxMinDto
 import numpy as np
+from itertools import groupby
+import pandas as pd
 
 
-def buidlNewHead(df, indexs):
-    rows = df.iloc[indexs]
+def concatRows(df, indexs, spliter = " "):
+    rows = df[df.index.isin(indexs)]
 
     heads = []
     for ind, column in enumerate(rows.columns):
         incoms = rows[column].tolist()
-        incoms = [str(incom) for incom in incoms if str(incom) != "nan"]
-        head = " ".join(incoms)
+        incoms = [str(incom).replace("=", "") for incom in incoms if str(incom) != "nan"]
+        head = spliter.join(incoms)
         heads.append(head)
 
     return heads
+
+
+def merge(df):
+    nan_values = df[df.isna().any(axis=1)]
+    Lnan = list(nan_values.index.values)
+    if len(Lnan) > 1:
+        maxmin = []
+        for k, g in groupby(enumerate(Lnan), lambda x: x[0] - x[1]):
+            items = [i[1] for i in g]
+            if len(items) > 1:
+                maxmin.append(MaxMinDto(min=items[0], max=items[-1]))
+
+        if len(maxmin) > 0:
+            for mm in maxmin:
+                indexs = list(range(mm.min, mm.max + 1))
+                row = concatRows(df, indexs, " ")
+
+                if (len(row) == len(df.columns)):
+                    replace = {}
+                    for ind, column in enumerate(df.columns):
+                        replace[column] = row[ind]
+                    
+                    df.loc[mm.min] = replace
+
+                    indexs.pop(0)
+                    df = df.drop(indexs)
+                
+    return df
 
 
 def transform(df):
@@ -30,11 +61,11 @@ def transform(df):
         # NOTE: Из всех что нашли предположим это
         # были реальные заголовки - сформируем и заменим
         if len(LDrop) > 0:
-            heads = buidlNewHead(df, LDrop)
+            heads = concatRows(df, LDrop)
             if len(heads) == Ncolumn:
                 Cnames = {}
                 for ind, column in enumerate(df.columns):
-                    Cnames[column] = heads[ind]
+                    Cnames[column] = "Unnamed" not in column and "%s %s" % (column, heads[ind]) or heads[ind]
                 df = df.rename(columns=Cnames)
 
         # NOTE: Все строки с заполненностью
@@ -54,6 +85,7 @@ def NaN(dataframe):
     df = df.replace(r"\s+", " ", regex=True)
     df = df.dropna(axis=1, how="all")
     df = transform(df)
+    df = merge(df)
     df = df.replace(np.nan, "-", regex=True)
     return df
 
